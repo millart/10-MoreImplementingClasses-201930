072152b269899eff4f54fb91a28b500dc7aa09fe
millart <millart@rose-hulman.edu>
2019-03-26 21:12:14 -0400

    DONE

--- src/m1_Line.py
+++ src/m1_Line.py
@@ -217,7 +217,7 @@ class Line(object):
           :type end:   Point
         """
         # ---------------------------------------------------------------------
-        # TODO: 3.
+        # DONE: 3.
         #   a. READ the above specification, including the Example.
         #        ** ASK QUESTIONS AS NEEDED. **
         #        ** Be sure you understand it, ESPECIALLY the Example.
